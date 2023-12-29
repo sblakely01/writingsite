c918fea9c6565ea4de08532d7f0cf1c877934e52
stephanye <stephanye.blakely@gmail.com>
2023-12-29 02:36:44 -0500

    Fixed Sendgrid Sender

--- pages/contact.js
+++ pages/contact.js
@@ -55,12 +55,6 @@ export default function ContactUs() {
  
    const handleSubmit = async (e) => {
      e.preventDefault();
-
-
-     await setFullname(e.target[0].value);
-     await setEmail(e.target[1].value);
-     await setSubject(e.target[2].value);
-     await setMessage(e.target[3].value);
  
      let isValidForm = handleValidation();
  
@@ -85,11 +79,17 @@ export default function ContactUs() {
          setShowSuccessMessage(false);
          setShowFailureMessage(true);
          setButtonText("Send");
+
          return;
        }
        setShowSuccessMessage(true);
        setShowFailureMessage(false);
        setButtonText("Send");
+       // Reset form fields
+       setFullname("");
+       setEmail("");
+       setMessage("");
+       setSubject("");
      }
      console.log(fullname, email, subject, message);
    };
@@ -142,16 +142,29 @@ export default function ContactUs() {
       <h1 className="text-2xl font-bold dark:text-gray-50">Contact Me</h1>
 
       <label htmlFor="fullname" className="text-gray-500 font-light mt-8 dark:text-gray-50">Full name<span className="text-red-500 dark:text-gray-50">*</span></label>
-      <input type="text" name="fullname" className={styles.field} />
+      <input type="text" name="fullname" value={fullname} onChange={(e) => { setFullname(e.target.value)}} className={styles.field} />
+        {errors?.fullname && (
+            <p className="text-red-500">Full name cannot be empty.</p>
+        )}
 
       <label htmlFor="email" className="text-gray-500 font-light mt-4 dark:text-gray-50">E-mail<span className="text-red-500">*</span></label>
-      <input type="email" name="email" className={styles.field} />
+      <input type="email" name="email" value={email} onChange={(e) => { setEmail(e.target.value)}} className={styles.field} />
+            {errors?.email && (
+                <p className="text-red-500">Email cannot be empty.</p>
+            )}
 
       <label htmlFor="subject" className="text-gray-500 font-light mt-4 dark:text-gray-50">Subject<span className="text-red-500">*</span></label>
-      <input type="text" name="subject" className={styles.field} />
+      <input type="text" name="subject" value={subject} onChange={(e) => { setSubject(e.target.value)}} className={styles.field} />
+                {errors?.subject && (
+                    <p className="text-red-500">Subject cannot be empty.</p>
+                )}
 
       <label htmlFor="message" className="text-gray-500 font-light mt-4 dark:text-gray-50">Message<span className="text-red-500">*</span></label>
-      <textarea name="message" className={styles.fieldMessage}></textarea>
+      <textarea name="message" value={message} onChange={(e) => { setMessage(e.target.value)}} className={styles.fieldMessage}></textarea>
+      {errors?.message && (
+          <p className="text-red-500">Message cannot be empty.</p>
+      )}
+      
       <div className="flex flex-row items-center justify-start">
         <button className={styles.button}>
           {buttonText}
@@ -159,8 +172,18 @@ export default function ContactUs() {
             <path d="M9.00967 5.12761H11.0097C12.1142 5.12761 13.468 5.89682 14.0335 6.8457L16.5089 11H21.0097C21.562 11 22.0097 11.4477 22.0097 12C22.0097 12.5523 21.562 13 21.0097 13H16.4138L13.9383 17.1543C13.3729 18.1032 12.0191 18.8724 10.9145 18.8724H8.91454L12.4138 13H5.42485L3.99036 15.4529H1.99036L4.00967 12L4.00967 11.967L2.00967 8.54712H4.00967L5.44417 11H12.5089L9.00967 5.12761Z" fill="currentColor" />
           </svg>
         </button>
-        {showSuccessMessage && successMessage}
-        {showFailureMessage && failureMessage}
+        </div>
+        <div className="text-left">
+            {showSuccessMessage && (
+                <p className="text-green-500 font-semibold text-sm my-2">
+                    Thank you! Your message was delivered.
+                </p>
+            )}
+            {showFailureMessage && (
+                <p className="text-red-500">
+                    Something went wrong, please try again.
+                </p>
+            )}
         </div>
       </div>
     </form>
